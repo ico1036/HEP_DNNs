9f550a334aac1f47c5515bc78ddd7e83d589d2a3
ico1036 <jiwoong.kim@cern.ch>
2019-12-05 12:54:33 +0000

    figedit

--- evaluation/Score.py
+++ evaluation/Score.py
@@ -1,6 +1,14 @@
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+Xsec_sig = 0.004942
+Xsec_bkg = 0.02353
+Gen_sig  = 443712
+Gen_bkg  = 444288
+Lumi	 = 35900
 
 path='../data/data_normal_by_all/'
 
@@ -22,18 +30,34 @@ hist_pred_sig = y_pred[idx_sig]
 hist_pred_bkg = y_pred[idx_bkg]
 
 
+weight_sig = np.ones(hist_pred_sig.shape) * Xsec_sig / Gen_sig * Lumi
+weight_bkg = np.ones(hist_pred_bkg.shape) * Xsec_bkg / Gen_bkg * Lumi
+
+
+
 plt.rc('xtick',labelsize=20)
 plt.rc('ytick',labelsize=20)
+plt.rcParams["figure.figsize"] = (8,8)
 
 
 bins = np.linspace(0,1,100)
-plt.hist(hist_pred_sig,bins=bins,color='r',alpha=0.7)
-plt.hist(hist_pred_bkg,bins=bins,color='b',alpha=0.7)
-plt.xlabel('DNN score')
-plt.ylabel('Nuber of events')
+normed_hist_sig = plt.hist(hist_pred_sig,bins=bins,color='r',histtype='step',linewidth=3,weights=weight_sig,label='DNN signal')
+normed_hist_bkg = plt.hist(hist_pred_bkg,bins=bins,color='b',histtype='step',linewidth=3,weights=weight_bkg,label='DNN background')
+plt.xlabel('DNN score',fontsize=25)
+plt.ylabel('Nuber of events',fontsize=25)
+plt.legend(prop={'size':15})
 
 #plt.xscale('log')
 #plt.yscale('log')
 
-plt.savefig("score.png")
+plt.grid(which='major', linestyle='-')
+plt.minorticks_on()
+
+plt.tight_layout()
+
+print(hist_pred_sig)
+print(normed_hist_sig)
+
+
+plt.savefig("normed_score.png")
 
---
+++ training/epoch_loss.py
@@ -0,0 +1,50 @@
+import matplotlib
+matplotlib.use('Agg')
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Path
+dir_   ='train_log.csv'
+
+# csv to dataframe
+df   = pd.read_csv(dir_)
+
+plt.rc('xtick',labelsize=30)
+plt.rc('ytick',labelsize=30)
+x_loss = np.arange(len(df['loss']))
+x_acc = np.arange(len(df['acc']))
+
+# --Epoch vs loss
+fig,axs = plt.subplots(2,1,figsize=(20,20))
+#axs[0].get_xaxis().get_major_formatter().set_useOffset(False)
+#axs[0].get_yaxis().get_major_formatter().set_useOffset(False)
+
+axs[0].plot(x_loss + 1,df['loss'],'--bo',color='darkorange',linewidth=4,markersize=10,label='Train-set')
+axs[0].plot(x_loss + 1,df['val_loss'],'--bo',color='royalblue',linewidth=4,markersize=10,label='Validation-set')
+
+#axs[0].set_yscale('log')
+axs[0].set_title('epoch vs loss',fontsize=50)
+axs[0].set_xlabel('epoch',fontsize=45)
+axs[0].set_ylabel('loss',fontsize=45)
+axs[0].legend(prop={'size':40})
+axs[0].grid(which='major', linestyle='-')
+axs[0].minorticks_on()
+
+# --Epoch vs acc
+#axs[1].get_xaxis().get_major_formatter().set_useOffset(False)
+#axs[1].get_yaxis().get_major_formatter().set_useOffset(False)
+
+axs[1].plot(x_acc + 1,df['acc'],'--bo',color='darkorange',linewidth=4,markersize=10,label='Train-set')
+axs[1].plot(x_acc + 1,df['val_acc'],'--bo',color='royalblue',linewidth=4,markersize=10,label='Validation-set')
+
+#axs[1].set_yscale('log')
+axs[1].set_title('epoch vs acc',fontsize=50)
+axs[1].set_xlabel('epoch',fontsize=45)
+axs[1].set_ylabel('loss',fontsize=45)
+axs[1].legend(prop={'size':40})
+axs[1].grid(which='major', linestyle='-')
+axs[1].minorticks_on()
+
+fig.tight_layout()
+plt.savefig('monitoring.png')
